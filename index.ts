35bc0e2d1a8dd0ab35788f2eafaf88a4e8e66ddd
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2024-05-24 15:34:05 -0300

    Update vercel config

--- index.ts
+++ index.ts
@@ -7,15 +7,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const mongoURI = process.env.MONGODB_URI || '';
+// const mongoURI = process.env.MONGODB_URI || '';
 
-mongoose.connect(mongoURI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+// mongoose.connect(mongoURI)
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB:', error);
+//   });
 
 app.use(express.json());
 
